53f7c8d27dff352804fb069bb975b382961afa86
egorowp <egorowp@gmail.com>
2017-07-26 12:43:14 +0300

    clean global error handler

--- NetCoreTemplate.Web/Scripts/services/global-error-handler.service.ts
+++ NetCoreTemplate.Web/Scripts/services/global-error-handler.service.ts
@@ -1,7 +1,5 @@
-﻿import { ErrorHandler, Injectable, Injector, OnInit } from '@angular/core';
+﻿import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
-//import { LoggingService } from '../services';
-//import * as StackTrace from 'stacktrace-js';
 import { AlertService } from '../services/alert.service';
 
 @Injectable()
@@ -11,21 +9,11 @@ export class GlobalErrorHandler implements ErrorHandler {
     }
 
     handleError(error) {
-        //const loggingService = this.injector.get(LoggingService);
         const location = this.injector.get(LocationStrategy);
         const message = error.message ? error.message : error.toString();
         const url = location instanceof PathLocationStrategy
             ? location.path() : '';
-        // get the stack trace, lets grab the last 10 stacks only
-        //StackTrace.fromError(error).then(stackframes => {
-        //    const stackString = stackframes
-        //        .splice(0, 20)
-        //        .map(function (sf) {
-        //            return sf.toString();
-        //        }).join('\n');
-        //    // log on the server
-        //    loggingService.log({ message, url, stack: stackString });
-        //});
+
         this.alertService.error(message);
         console.log({ message, url, stack: error.stack });
         throw error;
